07ebbc6bd164642234a510eb3abb76940f1f0b43
jpopa-po <82804778+jpopa-po@users.noreply.github.com>
2021-04-21 00:06:38 +0200

    Add files via upload

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/21 00:01:49 by marvin            #+#    #+#             */
+/*   Updated: 2021/04/21 00:01:49 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (to_find[j] == str[i + j])
+		{
+			if (to_find[j + 1] == '\0')
+			{
+				return (str + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+/*
+int	main(void)
+{
+	char str[] = "HOLALALAQUEJAJAYUPI";
+	char find[] = "YUPI";
+	printf("%s", ft_strstr(str, find));
+	return (0);
+}*/
